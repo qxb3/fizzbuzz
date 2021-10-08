32ec6d413e18becb03565bc9d009aacfbd61b1b4
Repyh <63894915+repyh@users.noreply.github.com>
2021-10-08 17:15:01 +0800

    Update fizzbuzz.js

--- codes/fizzbuzz.js
+++ codes/fizzbuzz.js
@@ -1,1 +1,6 @@
-for(let i = 1; i <= 100; i++) console.log(i % 15 ? 'FizzBuzz' : i % 3 ? 'Fizz' : i % 5 ? 'Buzz' : i);
+for(let i = 1; i <= 100; i++) {
+  if(i % 15 === 0) console.log('FizzBuzz');
+  if(i % 3 === 0) console.log('Fizz');
+  if(i % 5 === 0) console.log('Buzz');
+  else console.log(i)
+}
