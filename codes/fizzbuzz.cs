24445d1ad5725ddfaf63ef137101eefde44fd528
Repyh <63894915+repyh@users.noreply.github.com>
2021-10-08 17:03:42 +0800

    Create fizzbuzz.cs

---
+++ codes/fizzbuzz.cs
@@ -0,0 +1,7 @@
+class FizzBuzz {
+  static void Main(string[] args) {
+    for(int i = 0; i < 100; i++) {
+      Console.WriteLine(i % 15 ? "FizzBuzz" : i % 3 ? "Fizz" : i % 5 ? "Buzz" : i)
+    }
+  }
+}
