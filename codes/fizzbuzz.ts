a4b8554f1db5dc4317961772e1f6fb5834134364
Repyh <63894915+repyh@users.noreply.github.com>
2021-10-08 17:14:30 +0800

    Update fizzbuzz.ts

--- codes/fizzbuzz.ts
+++ codes/fizzbuzz.ts
@@ -1,5 +1,6 @@
 for(let i: number = 1; i <= 100; i++) {
   if(i % 15 === 0) console.log('FizzBuzz');
   if(i % 3 === 0) console.log('Fizz');
-  if(i % 5 === 0) console.log('Buzz')
+  if(i % 5 === 0) console.log('Buzz');
+  else console.log(i)
 }
